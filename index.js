6b87ff8c98274540e33ecef8e23c4016236bda6f
DaveMitten <davidmitten88@gmail.com>
2017-06-05 10:30:56 +0100

    started controllers, front end auth

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const port    = process.env.PORT || 4000;
 const app     = express();
 const dest    = `${__dirname}/public`;
 
-app.use(express.static(dest));
+app.use('/api', expressJWT({ secret: secret })
+  .unless({
+    path: [
+      { url: '/api/login', methods: ['POST'] },
+      { url: '/api/register', methods: ['POST'] }
+    ]
+  }));
 
 app.get('/*', (req, res) => res.sendFile(`${dest}/index.html`));
 
---
+++ src/factories/user.factory.js
@@ -0,0 +1,10 @@
+angular
+.module('project4')
+.factory('User', userFactory);
+
+userFactory.$inject = ['API', '$resource'];
+function userFactory(API, $resource){
+  return $resource(`${API}/users/:id`, { id: '@_id'}, {
+    'register': { method: 'POST', url: `${API}/register` }
+  });
+}
